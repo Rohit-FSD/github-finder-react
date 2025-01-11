5926c90f1842f6681b46b41a359f2a4adc042b91
Rohit <rohit@Rohits-MacBook-Air.local>
2025-01-11 18:10:31 +0530

    React Hooks started and converted search comp. to use hooks

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,61 +1,50 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-export class Search extends Component {
-  state = {
-    text: "",
-  };
+const Search = ({ setAlertState, handleSearch, showClearBtn, clearUsers }) => {
+  const [text, setText] = useState("");
 
-  onChange = (e) => this.setState({ text: e.target.value });
+  const onChange = (e) => setText(e.target.value);
 
-  //If we have single input use above
-  //Else
-  //onChange = (e) => this.setState({ [e.target.name]: e.target.value });
-
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    if (!this.state.text)
-      this.props.setAlertState("Please enter something", "light");
+    if (!text) setAlertState("Please enter something", "light");
     else {
-      this.props.handleSearch(this.state.text);
-      this.setState({ text: "" });
+      handleSearch(text);
+      setText("");
     }
   };
 
-  onClearAction = () => this.props.clearUsers();
+  const onClearAction = () => clearUsers();
 
-  render() {
-    const IS_DISABLED = !this.state.text;
-    return (
-      <div>
-        <form onSubmit={this.onSubmit} className="form">
-          <input
-            type="text"
-            name="text"
-            placeholder="Search Users..."
-            value={this.state.text}
-            onChange={this.onChange}
-          />
-          <input
-            type="submit"
-            value="Search"
-            className="btn btn-dark btn-block"
-            // disabled={IS_DISABLED}
-            style={{ cursor: IS_DISABLED && "not-allowed" }}
-          />
-        </form>
-        {this.props.showClearBtn && (
-          <button
-            className="btn btn-light btn-block"
-            onClick={this.onClearAction}
-          >
-            Clear
-          </button>
-        )}
-      </div>
-    );
-  }
-}
+  const IS_DISABLED = !text;
+
+  return (
+    <div>
+      <form onSubmit={onSubmit} className="form">
+        <input
+          type="text"
+          name="text"
+          placeholder="Search Users..."
+          value={text}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+          // disabled={IS_DISABLED}
+          style={{ cursor: IS_DISABLED && "not-allowed" }}
+        />
+      </form>
+      {showClearBtn && (
+        <button className="btn btn-light btn-block" onClick={onClearAction}>
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
 
 Search.propTypes = {
   handleSearch: PropTypes.func.isRequired,
